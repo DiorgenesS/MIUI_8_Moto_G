80d3d46f6e6d562bcd69bc1ae8f9e4f4c67595ba
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-11 23:24:02 -0300

    added getprop for all variants thanks @princepaul

--- releasetools.py
+++ releasetools.py
@@ -5,15 +5,18 @@ def RemoveDeviceAssert(info):
   edify = info.script
   for i in xrange(len(edify.script)):
     if "ro.product" in edify.script[i]:
-      edify.script[i] = ''
+      edify.script[i] = """getprop("ro.product.device") == "xt1031" || getprop("ro.build.product") == "xt1031" || getprop("ro.product.device") == "xt1032" || getprop("ro.build.product") == "xt1032" || getprop("ro.product.device") == "xt1033" || getprop("ro.build.product") == "xt1033" || getprop("ro.product.device") == "xt1034" || getprop("ro.build.product") == "xt1034" || getprop("ro.product.device") == "falcon_umts" || getprop("ro.build.product") == "falcon_umts" || getprop("ro.product.device") == "falcon_umtsds" || getprop("ro.build.product") == "falcon_umtsds" || getprop("ro.product.device") == "falcon_cdma" || getprop("ro.build.product") == "falcon_cdma" || getprop("ro.product.device") == "falcon_retuaws" || getprop("ro.build.product") == "falcon_retuaws" || getprop("ro.product.device") == "falcon" || getprop("ro.build.product") == "falcon" || getprop("ro.product.device") == "falcon_gpe" || getprop("ro.build.product") == "falcon_gpe" || abort("This package is for device: xt1031,xt1032,xt1033,xt1034,falcon_umts,falcon_umtsds,falcon_cdma,falcon_retuaws,falcon,falcon_gpe; this device is " + getprop("ro.product.device") + ".");
+getprop("ro.bootloader") == "0x4118" || getprop("ro.bootloader") == "0x4119" || getprop("ro.bootloader") == "0x411A" || abort("This package supports bootloader(s): 0x4118, 0x4119, 0x411A; this device has bootloader " + getprop("ro.bootloader") + ".");
+unmount("/data");
+unmount("/system");"""
       return
 
 def AddAssertions(info):
-    edify = info.script
-    for i in xrange(len(edify.script)):
-        if " ||" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
-            edify.script[i] = edify.script[i].replace(" ||", ' || getprop("ro.build.product") == "falcon" || getprop("ro.product.device") == "falcon_umtsds" || getprop("ro.build.product") == "falcon_umtsds" ||')
-            return
+   edify = info.script
+   for i in xrange(len(edify.script)):
+    if " ||" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
+      edify.script[i] = edify.script[i].replace(" ||", ' ')
+      return
 
 def AddArgsForFormatSystem(info):
   edify = info.script
@@ -31,7 +34,7 @@ def WritePolicyConfig(info):
 
 def FullOTA_InstallEnd(info):
     WritePolicyConfig(info)
-    AddAssertions(info)
+    RemoveDeviceAssert(info)
 
 def IncrementalOTA_InstallEnd(info):
-    AddAssertions(info)
+    RemoveDeviceAssert(info)
