28736419c48a1159aebbf7ba9fe3b98a1dbc6cd1
Diorgenes <dyogenes_gd@hotmail.com>
2017-04-13 11:43:37 -0300

    Added logo for Moto G Thanks Prince Paul Signed-off-by: Diorgenes <dyogenes_gd@hotmail.com>

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -61,3 +61,8 @@ fi
 if [ $1 = "Settings" ];then
     applyPatch $1 $2
 fi
+
+if [ $1 = "XiaomiAccount" ];then
+    applyPatch $1 $2
+	sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
+fi
